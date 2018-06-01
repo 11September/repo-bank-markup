0ee71a34d9fd35a067fc859216f248eeb8e8838c
Stanislav <shiptya@icloud.com>
2018-06-01 19:40:26 +0300

    some refactor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,124 @@
+particlesJS('particles-js',
+    {
+        "particles": {
+            "number": {
+                "value": 45,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#6e0000"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "bubble"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+        "config_demo": {
+            "hide_card": false,
+            "background_color": "#fff",
+            "background_image": "",
+            "background_position": "50% 50%",
+            "background_repeat": "no-repeat",
+            "background_size": "cover"
+        }
+    }
+
+);
+
+
 $(document).ready(function() {
     $(".mobile-btn").click(function () {
         $(".menu-list").slideToggle();
@@ -20,12 +141,6 @@ $(document).ready(function() {
 	});
 
 
-    // $(".pulse").click(function () {
-    //     $(".video-container").slideToggle();
-    //     $(".video-container").removeClass('hider');
-    // });
-
-
     $(".language-list").animate({width: 'toggle'});
     $(".arrow-lang").click(function () {
         $(".language-list").css({width: 'auto'});
@@ -34,5 +149,344 @@ $(document).ready(function() {
         $(".arrow").toggleClass("hider");
         $(".cross").toggleClass("hider");
     });
+
+
+
+
+    $('.overdrafts_slider').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        initialSlide: 12,
+        dots: true,
+        focusOnSelect: true,
+        slidesToShow: 9,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.wrapper-team-slider').slick({
+        centerMode: false,
+        centerPadding: '0px',
+        dots: true,
+        focusOnSelect: false,
+        prevArrow: "<img class='slick-prev' src='images/team-left.png'>",
+        nextArrow: "<img class='slick-next' src='images/team-right.png'>",
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+
+            {
+                breakpoint: 567,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.our_vision_slider_panel').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        dots: true,
+        arrows: true,
+        prevArrow: "<img class='slick-prev' src='images/arrow_left.png'>",
+        nextArrow: "<img class='slick-next' src='images/arrow_right.png'>",
+        focusOnSelect: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '200px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '100px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '80px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '10px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.roadmap_slider').slick({
+        centerMode: true,
+        centerPadding: '400px',
+        dots: true,
+        focusOnSelect: true,
+        infinite: true,
+        rows: 1,
+        slidesToShow: 3,
+        initialSlide: 2,
+        responsive: [
+            {
+                breakpoint: 1920,
+                settings: {
+                    centerPadding: '100px'
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    centerPadding: '250px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '150px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '80px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    centerPadding: '15px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '0px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $.fn.rotateImg = function (options) {
+        var defaults = {deg: 0};
+        var settings = $.extend({}, defaults, options);
+        return this.each(function () {
+            var img = $(this);
+            var imgpos = img.position();
+            var x0, y0;
+            $(window).load(function () {
+                x0 = imgpos.left + img.width() / 2;
+                y0 = imgpos.top + img.height() / 2;
+            });
+            var x, y, x1, y1, r;
+            $("html").mousemove(function (e) {
+                x1 = e.pageX;
+                y1 = e.pageY;
+                x = x1 - x0;
+                y = y1 - y0;
+                r = 175 + settings.deg - 175 / Math.PI * Math.atan2(y, x);
+                img.css("transform", "rotate(-" + r + "deg)");
+                img.css("-moz-transform", "rotate(-" + r + "deg)");
+                img.css("-webkit-transform", "rotate(-" + r + "deg)");
+                img.css("-o-transform", "rotate(-" + r + "deg)")
+            })
+        })
+    };
+
+    function rand(a) {
+        return 50 + Math.floor(Math.random() * (a - 200))
+    }
+
+    var w = $(window).width(), h = $(window).height();
+    $.each(Array(1), function (indx, element) {
+        $('.arrow_c').appendTo('#compas')
+    });
+    $('.arrow_c').rotateImg({deg: 0});
 });
 
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+var element = Number("0");
+$('.roadmap_slider_item').each(function () {
+    element = Number(element) + Number("1");
+    $(this).find('.roadmap_number').text(element);
+});
+
+$('.modal_2').on('shown.bs.modal', function (e) {
+    $('.overdrafts_slider').resize();
+});
+
+$('#rC_A').redCountdown({
+    end: '1388468325',
+    now: '1378441323',
+    labels: true,
+    style: {
+        element: "",
+        textResponsive: .5,
+        daysElement: {
+            gauge: {
+                thickness: .03,
+                bgColor: "rgba(0,0,0,0.05)",
+                fgColor: "white"
+            },
+            textCSS: 'font-family:\'OpenSans-Regular\'; font-size:25px; font-weight:300; color:#34495e;'
+        },
+        hoursElement: {
+            gauge: {
+                thickness: .03,
+                bgColor: "rgba(0,0,0,0.05)",
+                fgColor: "white"
+            },
+            textCSS: 'font-family:\'OpenSans-Regular\'; font-size:25px; font-weight:300; color:#34495e;'
+        },
+        minutesElement: {
+            gauge: {
+                thickness: .03,
+                bgColor: "rgba(0,0,0,0.05)",
+                fgColor: "white"
+            },
+            textCSS: 'font-family:\'OpenSans-Regular\'; font-size:25px; font-weight:300; color:#34495e;'
+        },
+        secondsElement: {
+            gauge: {
+                thickness: .03,
+                bgColor: "rgba(0,0,0,0.05)",
+                fgColor: "white"
+            },
+            textCSS: 'font-family:\'OpenSans-Regular\'; font-size:25px; font-weight:300; color:#34495e;'
+        }
+    },
+    onEndCallback: function () {
+        console.log("Time out!");
+    }
+});
+
+$(function () {
+    $("#sampleRedCountdownContainer").redCountdown({preset: "flat-colors", end: 1414821973});
+});
+
+$(".blue-bg").hover(function () {
+    $(this).css("transform", "rotateY(-180deg)");
+    $(this).css("perspective", "1000");
+    $(this).css("transition", "0.7s");
+    $(this).css("transform-style", "preserve-3d");
+    $(this).css("cursor", "pointer");
+    $(this).css("animation-direction", "alternate");
+}, function () {
+    $(this).css("transform", "rotateY(180deg)");
+    $(this).css("perspective", "1000");
+    $(this).css("transition", "0.9s");
+    $(this).css("transform-style", "preserve-3d");
+    $(this).css("cursor", "pointer");
+    $(this).css("animation-direction", "alternate-reverse");
+});
+
+Morris.Donut({
+    element: 'graph',
+    data: [
+        {value: 70, label: 'foo', formatted: 'at least 70%'},
+        {value: 15, label: 'bar', formatted: 'approx. 15%'},
+        {value: 10, label: 'baz', formatted: 'approx. 10%'},
+        {value: 5, label: 'A really really long label', formatted: 'at most 5%'}
+    ],
+    formatter: function (x, data) {
+        return data.formatted;
+    }
+});
+
+Morris.Donut({
+    element: 'graph-number2',
+    data: [
+        {value: 70, label: 'foo', formatted: 'at least 70%'},
+        {value: 15, label: 'bar', formatted: 'approx. 15%'},
+        {value: 10, label: 'baz', formatted: 'approx. 10%'},
+        {value: 5, label: 'A really really long label', formatted: 'at most 5%'}
+    ],
+    formatter: function (x, data) {
+        return data.formatted;
+    }
+});
